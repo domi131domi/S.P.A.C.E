1379f5d542a8e99be36b4805cf6740d56058c5df
domi131domi <d.szaci@gmail.com>
2019-03-21 19:44:33 +0100

    anims and ideas
    Animated player movent, some additional drawings and ideas

---
+++ Unity/S.P.A.C.E/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator animator;
+    public float rotationSpeed;
+    public float movementSpeed;
+    private bool engineON = false;
+
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float forward = Input.GetAxis("Vertical");
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        if (forward < 0)
+            forward = 0;
+        gameObject.transform.Rotate(0, 0, -rotationSpeed * horizontalMovement);
+        rb.AddRelativeForce((new Vector3(0,1,0)) * movementSpeed * forward);
+        if (forward != 0)
+            engineON = true;
+        else
+            engineON = false;   
+    }
+
+     void Update()
+    {
+        animator.SetBool("EngineOFF", !engineON);
+    }
+}
